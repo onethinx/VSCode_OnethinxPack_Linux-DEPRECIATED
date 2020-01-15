18aa273673bdb21ea67d6e0529b850126568f2e2
Rolf <rolf@rolf-mac-pro-2.home>
2020-01-15 21:55:28 +0100

    update openocd.tcl for non-kitprog

--- config/scripts/openocd.tcl
+++ config/scripts/openocd.tcl
@@ -1,7 +1,7 @@
 source [find interface/${PROGRAMMER}.cfg]
 transport select swd
 source [find target/psoc6.cfg]
-kitprog3 power_config on 3300
+if {$PROGRAMMER == "kitprog3"} { kitprog3 power_config on 3300 }
 if {![info exists ENABLE_CM0] || ([info exists ENABLE_CM0] && ${ENABLE_CM0} != 0) } {
     ${TARGET}.cm0 configure -gdb-port disabled
 }
